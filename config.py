6f92141e08b2337d25f9622fa1fb9ad870f32ebd
aquib116 <rockygaming85400@gmail.com>
2024-08-26 15:45:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "https://telegram.me/+DuYhzuVBL4FhYjRl")
+DB_URI = environ.get("DB_URI", "mongodb+srv://rockygaming85400:1e3YXUXrFTUKuAZh@cluster0.o689d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
